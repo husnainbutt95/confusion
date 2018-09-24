dcc2d99d9313d2a8c22a3af5b19ace507c82f174
Husnainbutt95 <rocking.butt95@gmail.com>
2018-09-25 01:11:55 +0500

    End of module one

---
+++ src/components/DishDetailComponent.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, ListGroup, ListGroupItem } from 'reactstrap';
+
+
+class DishDetail extends Component{
+    constructor(props){
+        super(props);
+    }
+    renderComments(dishComments){
+        if(dishComments != null){
+           const dishComment = dishComments.map((com) => {
+               return (
+                <ListGroup key = {com.id}>
+                    <ListGroupItem>{com.comment}<br/>--{com.author} {com.date} </ListGroupItem><br/>
+                </ListGroup>
+               );
+           });
+            return dishComment;
+        }
+        else{
+            return (<div></div>);
+        }
+    }
+    render(){
+        if (this.props.dish != null){
+            return(
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <Card>
+                            <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
+                            <CardBody>
+                            <CardTitle>{this.props.dish.name}</CardTitle>
+                            <CardText>{this.props.dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <h4>Comments</h4>
+                        {this.renderComments(this.props.dish.comments)}            
+                    </div>
+                </div>
+            );
+            }else{
+            return(
+                <div></div>
+            );
+        }
+}
+}
+export default DishDetail;
