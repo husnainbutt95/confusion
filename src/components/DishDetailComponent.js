b4245ba7eefb4b565bb449d31cdc2c120ff6a8bb
Husnainbutt95 <rocking.butt95@gmail.com>
2018-10-30 12:33:33 +0500

    Fetch with reducer'

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { LocalForm, Control, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -85,7 +86,7 @@ class CommentForm extends Component {
             return (
                 <React.Fragment>
                     <Card>
-                        <CardImg top src={dish.image} alt={dish.name} />
+                        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
